f9f441676b8770ffb1d42e1bec086dc530a525c9
Qlammm <87619762+Qlammm@users.noreply.github.com>
2021-07-19 04:06:22 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'alo Tuấn Huy ',
+    introDesc: `thấy lạ lắm đúng hong , hong bít sao tui lại gửi cái này cho ông chứ gì`,
+    btnIntro: 'nhấn đây gùi bít ',
+    title: 'ta daaaaaaaa ngạc nhiên chưaaaaaaaaaa ',
+    desc: 'tui dethuong lắm đúng hokkkkkkk , trả lời thật lòng nhaaaa ',
+    btnYes: 'siêu dethuong lun ',
+    btnNo: 'hong hè ',
+    question:'đố bít sao dethuong được z đó ',
+    btnReply:' gửi cho Qlam nè  ',
+    reply: 'tại vì giống thiên thần á  ',
+    mess: 'tui bít mò  hihiiiii',
+    messDesc: 'nói chiện với tui nhìu lên đi cảnh cáo đó ',
+    btnAccept: 'Okiiiii lun ,
+    messLink: 'http://fb.com' // https://www.facebook.com/quynhhlam.06/
 }
