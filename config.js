1d8a3ca26e596d90c8774e4d965f670e2314d23e
Qlammm <87619762+Qlammm@users.noreply.github.com>
2021-07-19 04:19:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hi Tuấn Huyyy ',
+    introDesc: `chắc Tuấn Huy sẽ hong hỉu tại sao tui gửi cái này đúng hămmmm `,
+    btnIntro: 'nhấn đi gùi bít nhoooo ',
+    title: 'taaaa daaaaaaa trả lời câu hỏiiiii ',
+    desc: ' tui dễ thương lắm đúng honggg  ',
+    btnYes: 'chứ sao nữa ',
+    btnNo: ' thấy ghét ',
+    question:'biết lí do tại sao dễ thương như thế khônggggg ',
+    btnReply: 'nhấn vào gửi cho tuiiii ',
+    reply: 'bà dễ thương không cần lí dooooo ',
+    mess: ' waooooo tui cũng nghĩ như Tuấn Huyyyy á ',
+    messDesc: 'nên là hãy nói chuyện với tui nhìu hơn nhé ',
+    btnAccept: 'Okiiiii lun :333 ',
+    messLink: 'http://fb.com' //https://www.facebook.com/quynhhlam.06/
 }
